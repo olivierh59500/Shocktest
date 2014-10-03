ac47e8fb3c0723f50bc49506cfb4ae393dcf10a9
NinJaSl0th <sl0th@force-sec.com>
2014-10-03 13:34:18 -0400

    Add some comments

--- shocktest.py
+++ shocktest.py
@@ -1,17 +1,27 @@
 #!/usr/bin/env python
 
+
+#Python script to try and find shellshock attempts in logs or files
+#Please feel free to modify the script and use it.
+#
+#As usual, my idea here is to make it simple.
+#
+#Author: NinjaSl0th
+#Twitter: @ninjasl0th
+
 import re
 import sys
 
+#Store the different shellshock regular expressions
 shellsh = {"\(\)\s*\{.+?\};","\(\)\s*\{\s*\(.*\)=>\\\\"}
 
 if len(sys.argv) <= 1:
   exit("Usage: shocktest.py access.log")
 
 else:
-  #print sys.argv[1]
   opfile = sys.argv[1]
   svfile = raw_input("Save contents if found? [y/n]: ")
+  #some error checking when opening the file
   try:
     i = open(opfile,'r')
   except:
